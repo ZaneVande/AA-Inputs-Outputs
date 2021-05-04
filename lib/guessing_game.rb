2f5ab7d3b59467a6cba0030ecdbacb47b320db47
Zane Vandergriff <zanevande@gmail.com>
2021-05-04 19:04:08 -0400

    Functioning Terminal Guessing Game

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,3 +1,46 @@
 class GuessingGame
+    def initialize(min, max)
+        @min = min
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false 
+    end
 
+    def num_attempts
+        @num_attempts
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(num)
+        @num_attempts += 1
+
+        if num == @secret_num
+            @game_over = true
+            puts "---------------"
+            print "you win"
+            puts
+        end
+
+        if num > @secret_num
+            puts "---------------"
+            print "too big"
+            puts
+        end
+
+        if num < @secret_num
+            puts "---------------"
+            print "too small"
+            puts
+        end
+    end
+
+    def ask_user
+        print "enter a number: "
+        num = gets.chomp.to_i
+        check_num(num)
+    end
 end
